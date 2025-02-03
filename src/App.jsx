e70bd6592721223c262e8c0bbc74c8af94acfef3
Alex <alexanderjannespia0127@gmail.com>
2025-02-03 19:32:07 +0800

    new changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,7 @@ import { useState } from 'react'
 import Home from './pages/Home.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (<Home />)
+	return (<Home />)
 }
 
 export default App
